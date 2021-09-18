7f628eea2401272869bcf94c6fefb756ebf69d31
Ketan Chaudhari <35309821+ketan-10@users.noreply.github.com>
2021-09-18 22:43:14 +0530

    Update AsyncAwaitTest.js

--- AsyncAwaitTest.js
+++ AsyncAwaitTest.js
@@ -1,3 +1,22 @@
+//////////////////////////////////////////////////////////
+
+async function myfunc(i) {
+  await new Promise(r => setTimeout(r, 3000));
+  console.log("Hello");
+  return new Promise(async (resolve, reject) => {
+    await new Promise(r => setTimeout(r, 1000));
+    // resolve(new Promise(r => setTimeout(() => {r(i)}, 5000)));
+    resolve(new Promise(r => setTimeout(r.bind(this,i), 5000)));
+    
+  });
+}
+
+myfunc(10).then((value) => {
+  console.log(value);
+});
+
+///////////////////////////////////////////////////////////////////////////
+
 console.log("start");
 
 async function test(){
