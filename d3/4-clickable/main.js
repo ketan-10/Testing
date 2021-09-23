7a6b8d0f66090269d164f511975d3f90ee29e3cb
wiwium <ketanchaudhari1998@gmail.com>
2021-09-23 20:40:51 +0530

    added clickable, and color change

--- d3/4-clickable/main.js
+++ d3/4-clickable/main.js
@@ -10,26 +10,36 @@ console.log("root Before: ",root);
 //   console.log("root After: ",root);
 // }, 1000);
 
-
+const container = d3.select("svg")
+  .attr("viewBox", [-100, -4000, 2000, 8000])
+  .append("g");
 
 const render = () => {
   tree(root);
-  const eachNode = d3.select("svg")
-    .attr("viewBox", [-100, -100, screen.width, 100])
-    .append("g")
-    .selectAll("text")
-    .data(root);
+  console.log("root After: ",root);
+  const eachNode = container 
+    .selectAll("g")
+    .data(root, (d) => d.data.tree_id);   // <- to identify which node got deleted.
+    // but we are giving all new data???
+    // no we are not re-drawing all tree,
+    // we are selecting existing nodes, and giving all nodes on that
+    // so let's say there are already 100 nodes, and i give 98 nodes again, how to know, which 2 are deleted
+    // or 5 were deleted and 3 added. 
   
-  // eachNode
-  //   .exit()
-  //   .remove();
+  eachNode
+    .exit()
+    .each(d => console.log(d))
+    .remove();
   
   eachNode
     .enter()
     .append("g")
-    .on("click", (_,d) => {
-      console.log("clicked: ",d.data)
-      d.children = null;
+    .on("click", (a,d) => {
+      console.log("A: ",a);
+      console.log("clicked: ",d)
+      d.data.children = undefined;
+      d.children = undefined;
+      d3.select(a.target).style("fill", "red")
       render();
     })
     .append("text")
